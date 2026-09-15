6ca7b761c3eca3be65bc68183ce0db13bba2b5af
sheetalbeck01 <sheetalbeck01@gmail.com>
2026-09-16 00:08:45 +0530

    remove duplicates sol2

--- leet-code/remove-duplicates-from-sorted-array.py
+++ leet-code/remove-duplicates-from-sorted-array.py
@@ -19,3 +19,25 @@ sol = Solution()
 
 print(sol.remove_duplicates([1,1,1,1,1,1,3,5,7,8,8,8,9]))
 
+# -------------------------------------------------------
+# but this solution creating new list without duplicates but in leetcode I've to rearrange nums
+class SolutionLeet:
+    def remove_duplicates(self, nums):
+        temp = nums[0]
+        unique = [nums[0]]
+        for i in range(1, len(nums)):
+            if temp != nums[i]:
+                unique.append(nums[i])
+                temp = nums[i]
+
+        k = 0
+        for i in range(len(unique)):
+            k+=1
+
+        
+        print(k, "nums=", unique)
+            
+
+sol2 = SolutionLeet()
+
+sol2.remove_duplicates([0,0,1,1,1,2,2,3,3,4])
